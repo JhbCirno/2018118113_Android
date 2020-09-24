f57a2898d3e5798788e6fbf20ceded0aa6ebb918
DESKTOP-LCOPULR\jinhanbo <1196958680@qq.com>
2020-09-24 10:04:27 +0800

    第三次作业

---
+++ Helloworldtest/app/src/main/java/com/example/helloworldtest/Firstactivity.java
@@ -0,0 +1,53 @@
+package com.example.helloworldtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class Firstactivity extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+      getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch(item.getItemId()){
+            case R.id.add_item:
+                Toast.makeText(this, "you click the add", Toast.LENGTH_SHORT).show();
+            case R.id.remove_item:
+                Toast.makeText(this,"you click the remove",Toast.LENGTH_SHORT).show();
+                break;
+            default:
+        }
+        return true;
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d("FirstActivity",this.toString());
+        setContentView(R.layout.first_layout);
+        Button button1 = (Button) findViewById(R.id.button);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View V) {
+               Intent intent = new Intent(Firstactivity.this,Firstactivity.class);
+            }
+        });
+        button1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(Firstactivity.this,secondactivity.class);
+                startActivity(intent);
+            }
+        });
+}
+}
---
+++ Helloworldtest/app/src/main/java/com/example/helloworldtest/secondactivity.java
@@ -0,0 +1,14 @@
+package com.example.helloworldtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class secondactivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_layout);
+    }
+}
