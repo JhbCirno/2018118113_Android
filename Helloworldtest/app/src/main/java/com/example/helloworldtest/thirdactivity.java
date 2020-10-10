e81fea986b9981fce710a1627e71d2bf08aee508
DESKTOP-LCOPULR\jinhanbo <1196958680@qq.com>
2020-10-10 20:21:54 +0800

    第三次作业（四种不同的启动模式）

--- Helloworldtest/app/src/main/java/com/example/helloworldtest/thirdactivity.java
+++ Helloworldtest/app/src/main/java/com/example/helloworldtest/thirdactivity.java
@@ -2,8 +2,8 @@ package com.example.helloworldtest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import android.util.Log;
+        import android.os.Bundle;
+        import android.util.Log;
 
 public class thirdactivity extends AppCompatActivity {
 
