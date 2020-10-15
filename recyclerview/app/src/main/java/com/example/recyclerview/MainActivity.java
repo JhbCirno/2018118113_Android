23529a14f296f04315eb6b2a0523bc44d47b6106
DESKTOP-LCOPULR\jinhanbo <1196958680@qq.com>
2020-10-15 21:31:32 +0800

    第四次作业（recyclerview）

--- recyclerview/app/src/main/java/com/example/recyclerview/MainActivity.java
+++ recyclerview/app/src/main/java/com/example/recyclerview/MainActivity.java
@@ -1,13 +1,14 @@
-package com.example.reclc;
+package com.example.recyclerview;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class secondactivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
     }
 }
