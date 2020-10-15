2588e2edbeb7ebb16ab3376dd18a9a1f450ef1d5
DESKTOP-LCOPULR\jinhanbo <1196958680@qq.com>
2020-10-15 19:42:03 +0800

    no message

--- Reclc/app/src/main/java/com/example/reclc/MainActivity.java
+++ Reclc/app/src/main/java/com/example/reclc/MainActivity.java
@@ -3,18 +3,38 @@ package com.example.reclc;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
-    private String[] data = {"a","b","c","d","a","b","c","d","a","b","c","d","a","b","c","d"};
+    private List<people> peopleList = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
-                MainActivity.this, android.R.layout.simple_list_item_1, data);
+        PeopleAdapter adapter = new PeopleAdapter(
+                MainActivity.this, R.layout.people_item, peopleList);
         ListView listView = (ListView)findViewById(R.id.list_view);
         listView.setAdapter(adapter);
     }
+    private void initPeople(){
+        for (int i=0;i<2;i++){
+            people max = new people("max",R.drawable.makesi);
+            peopleList.add(max);
+            people liening = new people("liening",R.drawable.liening);
+            peopleList.add(liening);
+            people enlai = new people("enlai",R.drawable.enlai);
+            peopleList.add(enlai);
+            people zedong = new people("zedong",R.drawable.maozedong);
+            peopleList.add(zedong);
+            people sidal = new people("sidal",R.drawable.sidalin);
+            peopleList.add(sidal);
+            people engesi = new people("engesi",R.drawable.engesi);
+            peopleList.add(engesi);
+        }
+    }
+
 }
