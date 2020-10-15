b96f70e2f72a44e26b356c419da7fab79c67b883
DESKTOP-LCOPULR\jinhanbo <1196958680@qq.com>
2020-10-15 21:02:37 +0800

    第四次作业（recyclerview）

--- Reclc/app/src/main/java/com/example/reclc/MainActivity.java
+++ Reclc/app/src/main/java/com/example/reclc/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.reclc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -20,7 +24,16 @@ public class MainActivity extends AppCompatActivity {
                 MainActivity.this, R.layout.people_item,peopleList);
         ListView listView = (ListView)findViewById(R.id.list_view);
         listView.setAdapter(adapter);
-    }
+        Log.d("MainActivaty",this.toString());
+        Button button1= (Button)findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,secondactivity.class);
+            }
+        });
+        }
+
     private void initPeople(){
         for (int i=0;i<1;i++){
             people max = new people("max",R.drawable.makesi);
---
+++ Reclc/app/src/main/java/com/example/reclc/PeopleAdapter2.java
@@ -0,0 +1,10 @@
+package com.example.reclc;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class PeopleAdapter2 extends RecyclerView.ViewHolder {
+    }
+}
