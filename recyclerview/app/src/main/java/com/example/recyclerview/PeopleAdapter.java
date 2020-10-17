81feaa07827e5c7dc44b9cd5f7a4c1152f14fb5d
DESKTOP-LCOPULR\jinhanbo <1196958680@qq.com>
2020-10-17 11:12:33 +0800

    第四次作业（Recyclerview）

---
+++ recyclerview/app/src/main/java/com/example/recyclerview/PeopleAdapter.java
@@ -0,0 +1,43 @@
+package com.example.recyclerview;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class PeopleAdapter extends RecyclerView.Adapter<PeopleAdapter.ViewHolder>{
+    private List<people> mpeopleList;
+    static class ViewHolder extends RecyclerView.ViewHolder{
+        ImageView peopleImage;
+        TextView peoplename;
+        public ViewHolder(View view){
+            super(view);
+            peopleImage=(ImageView)view.findViewById(R.id.people_image);
+            peoplename=(TextView)view.findViewById(R.id.people_text);
+        }
+    }
+    public PeopleAdapter(List<people> peopleList){
+        mpeopleList=peopleList;
+    }
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent,int viewType){
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.people_item,parent,false);
+        ViewHolder holder = new ViewHolder(view);
+        return holder;
+    }
+    @Override
+    public void onBindViewHolder(ViewHolder holder,int postion){
+        people people = mpeopleList.get(postion);
+        holder.peopleImage.setImageResource(people.getImageId());
+        holder.peoplename.setText(people.getName());
+    }
+    @Override
+    public int getItemCount(){
+        return mpeopleList.size();
+    }
+}
