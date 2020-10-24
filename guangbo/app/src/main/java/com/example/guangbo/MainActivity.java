c769b407cf071d7a8a0e3a29905bce5e1bb2182d
DESKTOP-LCOPULR\jinhanbo <1196958680@qq.com>
2020-10-24 10:40:01 +0800

    第五次作业（广播）

--- guangbo/app/src/main/java/com/example/guangbo/MainActivity.java
+++ guangbo/app/src/main/java/com/example/guangbo/MainActivity.java
@@ -2,13 +2,16 @@ package com.example.guangbo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.IntentFilter;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+private IntentFilter intentFilter;
+private NetworkChangeRecevier networkChangeRecevier;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        intentFilter =
     }
 }
