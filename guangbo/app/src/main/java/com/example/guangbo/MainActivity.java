214815cce1471dd038d33f9ae537aa2162aaac30
DESKTOP-LCOPULR\jinhanbo <1196958680@qq.com>
2020-10-24 10:48:59 +0800

    第五次作业（广播）

--- guangbo/app/src/main/java/com/example/guangbo/MainActivity.java
+++ guangbo/app/src/main/java/com/example/guangbo/MainActivity.java
@@ -2,8 +2,14 @@ package com.example.guangbo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
 import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 private IntentFilter intentFilter;
@@ -13,5 +19,17 @@ private NetworkChangeRecevier networkChangeRecevier;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         intentFilter =
+    }
+    class NetworkChangeRecevier extends BroadcastReceiver{
+        @Override
+        public void onReceive(Context context, Intent intent){
+            ConnectivityManager connectivityManager = (ConnectivityManager)getSystemService(context.CONNECTIVITY_SERVICE);
+            NetworkInfo networkInfo = connectionManager
+            if (networkInfo != null &&networkInfo.isAvailable()){
+                Toast.makeText(context,"network is available",Toast.LENGTH_SHORT).show();
+            }else{
+                Toast.makeText(context,"network is unavable",Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
