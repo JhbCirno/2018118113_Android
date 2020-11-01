66e33e84a852404fc458fec309abe921fff6d8d2
DESKTOP-LCOPULR\jinhanbo <1196958680@qq.com>
2020-11-01 10:58:13 +0800

    实验五（广播）

--- guangbo/app/src/main/java/com/example/guangbo/MainActivity.java
+++ guangbo/app/src/main/java/com/example/guangbo/MainActivity.java
@@ -3,22 +3,36 @@ package com.example.guangbo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.BroadcastReceiver;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private IntentFilter intentFilter;
-    private NetworkChangeRecevier networkChangeRecevier;
+   /*private NetworkChangeRecevier networkChangeRecevier;*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button)findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent("com.example.guangbo.MY_BROADCAST");
+                intent.setComponent(new ComponentName("com.example.guangbo","com.example.guangbo.MyBoradcastReceiver"));
+                sendBroadcast(intent);
+            }
+        });}
+/*
         intentFilter = new IntentFilter();
         intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
         networkChangeRecevier = new NetworkChangeRecevier();
@@ -39,5 +53,5 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(context,"network is unavable",Toast.LENGTH_SHORT).show();
             }
         }
-    }
+    }*/
 }
