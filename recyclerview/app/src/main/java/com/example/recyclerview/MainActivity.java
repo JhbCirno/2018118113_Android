1b168eedfc47fe1170122a293619dae9b9e18c56
DESKTOP-LCOPULR\jinhanbo <1196958680@qq.com>
2020-11-12 00:26:14 +0800

    第四次实验（Recyclerview和四种不同的布局）

--- recyclerview/app/src/main/java/com/example/recyclerview/MainActivity.java
+++ recyclerview/app/src/main/java/com/example/recyclerview/MainActivity.java
@@ -44,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this,Frame.class);
                 startActivity(intent);
-            }
+            
         });
 
 
