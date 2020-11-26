678e8a82b40f44da307b9e73dc57effbacf9f247
DESKTOP-LCOPULR\jinhanbo <1196958680@qq.com>
2020-11-26 21:14:27 +0800

    第六次作业（服务）

--- service/app/src/main/java/com/example/service/MainActivity.java
+++ service/app/src/main/java/com/example/service/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.service;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button startService = (Button)findViewById(R.id.button1);
+        Button stopService = (Button)findViewById(R.id.button2);
+        startService.setOnClickListener(this);
+        stopService.setOnClickListener(this);
+    }
+    public void onClick(View v){
+        switch(v.getId()){
+            case R.id.button1:
+                Intent startIntent = new Intent(this,MyService.class);
+                startActivity(startIntent);
+                break;
+            case R.id.button2:
+                Intent stopIntent = new Intent(this,MyService.class);
+                stopService(stopIntent);
+                break;
+        }
     }
 }
--- service/app/src/main/java/com/example/service/MyService.java
+++ service/app/src/main/java/com/example/service/MyService.java
@@ -3,6 +3,7 @@ package com.example.service;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.util.Log;
 
 public class MyService extends Service {
     public MyService() {
@@ -16,13 +17,16 @@ public class MyService extends Service {
     @Override
     public void onCreate(){
         super.onCreate();
+        Log.d("MyService","oncreate");
     }
     @Override
     public int onStartCommand(Intent intent,int flags,int startId){
+        Log.d("MyService","onStart");
         return super.onStartCommand(intent, flags, startId);
     }
     @Override
     public void onDestroy(){
         super.onDestroy();
+        Log.d("MyService","onDestroy");
     }
 }
