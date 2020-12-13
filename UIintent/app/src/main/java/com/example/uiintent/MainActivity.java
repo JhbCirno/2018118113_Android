3efcf0491aa5f4b25f4402571b39b217e3539321
DESKTOP-LCOPULR\jinhanbo <1196958680@qq.com>
2020-12-13 20:30:59 +0800

    第八次作业(UI)

--- UIintent/app/src/main/java/com/example/uiintent/MainActivity.java
+++ UIintent/app/src/main/java/com/example/uiintent/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -14,5 +17,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar1);
         setSupportActionBar(toolbar);
+    }
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.toolbar,menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch(item.getItemId()){
+            case R.id.backup:
+                Toast.makeText(this,"You clicked Backup",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.delete :
+                Toast.makeText(this,"You clicked Delete",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.setting :
+                Toast.makeText(this,"You clicked Setting",Toast.LENGTH_SHORT).show();
+                break;
+             default:
+        }
+        return true;
     }
 }
